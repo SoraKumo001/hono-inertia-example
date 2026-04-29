2a71c4d02428072b4db7c3d78a7254b5fa2d327d
SoraKumo <info@croud.jp>
2026-04-29 14:19:45 +0900

    feat: implement root view configuration using inertia and react server-side rendering

--- app/root-view.tsx
+++ app/root-view.tsx
@@ -1,19 +1,43 @@
-import { renderToString } from 'react-dom/server'
-import { Link, Script, ViteClient } from 'vite-ssr-components/react'
-import { serializePage, type PageObject, type RootView } from '@hono/inertia'
+import { renderToString, renderToReadableStream } from 'react-dom/server';
+import { Link, Script, ViteClient } from 'vite-ssr-components/react';
+import { type RootView } from '@hono/inertia';
+import {
+  createInertiaApp,
+  usePage,
+  type ResolvedComponent,
+} from '@inertiajs/react';
 
-const Document = ({ page }: { page: PageObject }) => (
-  <html>
-    <head>
-      <ViteClient />
-      <Link rel="stylesheet" href="/app/styles.css" />
-      <Script src="/app/client.tsx" />
-    </head>
-    <body>
-      <script data-page="app" type="application/json" dangerouslySetInnerHTML={{ __html: serializePage(page) }} />
-      <div id="app" />
-    </body>
-  </html>
-)
+type Page = ReturnType<typeof usePage>;
 
-export const rootView: RootView = (page) => '<!DOCTYPE html>' + renderToString(<Document page={page} />)
+const pages = import.meta.glob('./pages/**/*.tsx', { eager: true });
+
+export const rootView: RootView = async (page) => {
+  const res = await createInertiaApp({
+    page: page as Page,
+    render: renderToString,
+    resolve: async (name) => {
+      const pages = import.meta.glob<{ default: ResolvedComponent }>(
+        './pages/**/*.tsx',
+      );
+      const page = await pages[`./pages/${name}.tsx`]();
+      return page.default;
+    },
+    setup: ({ App, props }) => <App {...props} />,
+  });
+
+  const { head, body } = res;
+  return (
+    '<!DOCTYPE html>\n' +
+    renderToString(
+      <html>
+        <head>
+          <ViteClient />
+          <Link rel='stylesheet' href='/app/styles.css' />
+          <Script src='/app/client.tsx' />
+          <body dangerouslySetInnerHTML={{ __html: head }} />
+        </head>
+        <body dangerouslySetInnerHTML={{ __html: body }} />
+      </html>,
+    )
+  );
+};
